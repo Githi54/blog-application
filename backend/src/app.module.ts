3f8df9ab744badf0ee9c4c54c7ced2fa1d2f8809
Mykyta Sloboda <104434132+Githi54@users.noreply.github.com>
2025-01-13 21:43:45 +0200

    feature: init author & posts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { AuthorModule } from './author/author.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PostsModule, AuthorModule],
 })
 export class AppModule {}
