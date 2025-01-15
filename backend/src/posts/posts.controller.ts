307a852118c1732b575fb6d29787873a08f58f3d
Mykyta Sloboda <104434132+Githi54@users.noreply.github.com>
2025-01-15 14:34:04 +0200

    feature: init setup posts

--- backend/src/posts/posts.controller.ts
+++ backend/src/posts/posts.controller.ts
@@ -1,7 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  async getAllPosts() {
+    return this.postsService.getAllPosts();
+  }
+
+  @Get(':postId')
+  async getPostById(@Param('postId') postId: string) {
+    return this.postsService.getPostById(postId);
+  }
+
+  @Get('author/:authorId')
+  async getPostByAuthor(@Param('authorId') authorId: string) {
+    return this.postsService.getPostByAuthor(authorId);
+  }
+
+  @Post('create')
+  async createPost(@Body() postData: object) {
+    return this.postsService.createPost(postData);
+  }
+
+  @Patch(':postId')
+  async updatePost(@Param('postId') postId: string, @Body() postData: object) {
+    return this.postsService.updatePost(postId, postData);
+  }
+
+  @Delete(':postId')
+  async deletePost(@Param('postId') postId: string) {
+    return this.postsService.deletePost(postId);
+  }
 }
--- backend/src/posts/posts.service.ts
+++ backend/src/posts/posts.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  async getAllPosts() {}
+
+  async getPostById(postId: string) {}
+
+  async getPostByAuthor(authorId: string) {}
+
+  async createPost(postData: object) {}
+
+  async updatePost(postId: string, newPostData: object) {}
+
+  async deletePost(postId: string) {}
+}
